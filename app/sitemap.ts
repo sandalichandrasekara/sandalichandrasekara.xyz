34e6215f5a4331cb0a214c9a6f347087394a1293
Sandali <sandalichandrasekarat20@gmail.com>
2026-07-31 15:21:41 +0530

    Update baseurl section

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
-      images: [`${baseUrl}/images/profile.jpg`],
+      images: [`${baseUrl}/public/images/profile.jpg`],
     },
     {
       url: `${baseUrl}/about`,
