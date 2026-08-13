fdcb25e212a48d42feedb18fff85801a76190e6e
Sandali <sandalichandrasekarat20@gmail.com>
2026-08-13 11:56:03 +0530

    Use infinity logo in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,11 +18,11 @@ export default function Navbar() {
       <nav className="mx-auto flex max-w-2xl items-center justify-between px-6 py-5">
         <Link href="/" className="block">
           <Image
-            src="/images/avatar.jpg"
+            src="/images/logo.png"
             alt="Sandali Chandrasekara"
-            width={36}
-            height={36}
-            className="rounded-full object-cover"
+            width={44}
+            height={44}
+            className="object-contain"
             priority
           />
         </Link>
